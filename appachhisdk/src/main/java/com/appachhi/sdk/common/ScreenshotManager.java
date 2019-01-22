9e3b51aaebbf2c2fc2f67ad2942b384944c33722
Farmaan Elahi <farmaan@triveous.com>
2019-01-22 15:29:53 +0530

    Trying out different way of taking screenshot from react native

--- appachhisdk/src/main/java/com/appachhi/sdk/common/ScreenshotManager.java
+++ appachhisdk/src/main/java/com/appachhi/sdk/common/ScreenshotManager.java
@@ -3,8 +3,11 @@ package com.appachhi.sdk.common;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 import android.util.Log;
 import android.view.View;
@@ -22,6 +25,8 @@ import java.util.concurrent.Executors;
  */
 public class ScreenshotManager {
     private static ScreenshotManager instance;
+    private static boolean isRunningFromReactNative = false;
+    private Handler mainHandler = new Handler(Looper.myLooper());
     private Executor executor = Executors.newSingleThreadExecutor();
     @SuppressWarnings("FieldCanBeLocal")
     private String tag = "ScreenshotManager";
@@ -39,67 +44,90 @@ public class ScreenshotManager {
     }
 
     /**
+     * Called by the React Module and should not used by the caller.It adds a field to check if the
+     * code is running from React Native Bridge
+     *
+     * @param isRunningFromReactNative true if running from react native
+     */
+    public static void setIsRunningFromReactNative(boolean isRunningFromReactNative) {
+        ScreenshotManager.isRunningFromReactNative = isRunningFromReactNative;
+    }
+
+    /**
      * Take the screenshot and saves them
      *
      * @param activity   {@link Activity} where screen exist
      * @param screenName Name of the screen
      */
-    public void takeAndSave(@NonNull Activity activity, String screenName) {
-        View view = activity.getWindow().getDecorView();
-        takeAndSave(view, screenName);
+    public void takeAndSave(@NonNull Activity activity, final String screenName) {
+        final View view = activity.getWindow().getDecorView().findViewById(android.R.id.content);
+        if (isRunningFromReactNative) {
+            // Adding delay for react native screenshot
+            mainHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    takeAndSave(view, screenName);
+                }
+            }, 1000);
+        } else {
+            // Immediate screenshot
+            takeAndSave(view, screenName);
+        }
     }
 
     /**
-     * Take the screenshot and saves them
+     * Take the screenshot and saves them if and only if the view has been created atleast
      *
      * @param view       View for which screenshot is to be taken
      * @param screenName Name of the screen
      */
-    private void takeAndSave(@NonNull final View view, final String screenName) {
-        view.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-            @Override
-            public boolean onPreDraw() {
-                view.getViewTreeObserver().removeOnPreDrawListener(this);
-                Bitmap bitmap = takeScreenShot(view);
-                saveScreenshot(view.getContext().getApplicationContext(), bitmap, screenName);
-                return true;
-            }
-        });
+    private void takeAndSave(@Nullable final View view, final String screenName) {
+        if (view != null) {
+            view.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+                @Override
+                public boolean onPreDraw() {
+                    view.getViewTreeObserver().removeOnPreDrawListener(this);
+                    Bitmap bitmap = takeScreenShot(view);
+                    saveScreenshot(view.getContext().getApplicationContext(), bitmap, screenName);
+                    return true;
+                }
+            });
+        }
     }
 
     /**
      * Saves the {@link Bitmap} as {@link android.graphics.Bitmap.CompressFormat#PNG} in app
      * specific directory.All the operation here are done in worker thread to prevent UI thread being
-     * blocked due to File Write Operation.
+     * blocked due to File Write Operation.Screenshot will be saved only if the bitmap is not null
      * <p>
      * File Naming format is ScreeName-SomeNumber.PNG
      *
      * @param applicationContext Application Context
-     * @param bitmap             Bitmap to be saved
+     * @param bitmap             Bitmap to be saved which can be null
      * @param screenName         Screen Name
      */
     @WorkerThread
-    private void saveScreenshot(Context applicationContext, final Bitmap bitmap, String screenName) {
-        File folder = applicationContext.getExternalFilesDir("appachhi");
-        String fileName = String.format(Locale.ENGLISH, "%s-%d.png", screenName, SystemClock.elapsedRealtime());
-        final File file = new File(folder, fileName);
-        executor.execute(new Runnable() {
-            @Override
-            public void run() {
-                ScreenshotManager.this.saveScreenshot(bitmap, file);
-            }
-        });
+    private void saveScreenshot(Context applicationContext, @Nullable final Bitmap bitmap, String screenName) {
+        if (bitmap != null) {
+            File folder = applicationContext.getExternalFilesDir("appachhi");
+            String fileName = String.format(Locale.ENGLISH, "%s-%d.png", screenName, SystemClock.elapsedRealtime());
+            final File file = new File(folder, fileName);
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    saveScreenshot(bitmap, file);
+                }
+            });
+        }
     }
 
-    private void saveScreenshot(Bitmap bitmap, File file) {
+    private void saveScreenshot(@NonNull Bitmap bitmap, File file) {
         FileOutputStream fileOutputStream = null;
         try {
             fileOutputStream = new FileOutputStream(file);
             // Convert Image to PNG without loosing quality
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
             fileOutputStream.flush();
-            // Release the memory allocated to the bitmap
-            bitmap.recycle();
             Log.d(tag, String.format("ScreenshotManager saved at %s", file.getAbsolutePath()));
         } catch (IOException e) {
             Log.e(tag, "Failed to save the screenshot", e);
@@ -111,6 +139,8 @@ public class ScreenshotManager {
                     e.printStackTrace();
                 }
             }
+            // Release the memory allocated to the bitmap
+            bitmap.recycle();
         }
     }
 
